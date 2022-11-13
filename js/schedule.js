cb2154bb6acdd3cfeaea73526b79f66b04abac9f
Octavio Lima <octaviolimasan@gmail.com>
2022-11-12 21:15:45 -0300

    Atualização dos botões de agentamento, atualização do layout responsivo

--- js/schedule.js
+++ js/schedule.js
@@ -9,14 +9,16 @@ const scheduleWindow = document.querySelector("#schedule-window");
 const serviceWindow = document.querySelector("#service-window");
 const overviewWindow = document.querySelector("#overview-window");
 
-const inputClientName = document.querySelector("#input-client-name");
-const confirmClientInfoBtn = document.querySelector("#confirm-client-info-btn");
-const confirmScheduleBtn = document.querySelector("#confirm-schedule-btn");
-const confirmServiceBtn = document.querySelector("#confirm-service-btn");
+const linkContainer = document.querySelector(".links-container");
+const footer = document.querySelector("footer");
+const diagBoxSpace = document.querySelector("#diag-box-space");
+const footerSpacer = document.querySelector("#footer-spacer");
 
 /* Define um ID para cada prestador de acordo com a ordem da array
 isso facilita encontrar quem está sendo selecionado */
-addIdToEachWorker();
+availableWorkers.forEach((worker, index) => {
+    worker.setAttribute("id", index);
+});
 
 document.addEventListener("click", (e) => {
     targetEl = e.target;
@@ -25,86 +27,42 @@ document.addEventListener("click", (e) => {
     if (parentEl !== null){
         let parentID = parentEl.id;
         selectWorker(parentID);
-
-        // parentEl.classList.toggle("selected-worker");
-        // focusOnWorker(true)
     }
 })
 
-function focusOnWorker(focused) {
-    if (focused == true) {
-    }else{
-    }
-}
 
 function selectWorker(selectedID) {
-  if (currentWorkerSel == selectedID) {
-    currentWorkerSel = -1;
-    availableWorkers.forEach((worker, index) => {
-      worker.classList.remove("selected-worker");
-    })
-        
-    resetWindowPage();
-    bgBlur.classList.add("hidden")
-    appointmentWindow.classList.add("hidden")
-  } else {
-    focusOnWorker(true);
-    currentWorkerSel = selectedID;
-    availableWorkers.forEach((worker, index) => {
-      worker.classList.remove("selected-worker");
-    })
+    if (currentWorkerSel == selectedID) {
+        currentWorkerSel = -1;
+        availableWorkers.forEach((worker, index) => {
+            worker.classList.remove("selected-worker");
+        })
+            
+        switchBetweenWindows(-1);
+        bgBlur.classList.add("hidden")
+        appointmentWindow.classList.add("hidden")
+        linkContainer.classList.remove("hidden");
+        footer.classList.remove("hidden");
+        diagBoxSpace.classList.add("hidden");
+        footerSpacer.classList.remove("hidden");
+    } else {
+        currentWorkerSel = selectedID;
+        availableWorkers.forEach((worker, index) => {
+            worker.classList.remove("selected-worker");
+        })
 
-    resetWindowPage();
-    bgBlur.classList.remove("hidden")
-    appointmentWindow.classList.remove("hidden")
-    availableWorkers[selectedID].classList.add("selected-worker")
-  }
+        switchBetweenWindows(-1);
+        bgBlur.classList.remove("hidden")
+        appointmentWindow.classList.remove("hidden")
+        linkContainer.classList.add("hidden");
+        footer.classList.add("hidden");
+        availableWorkers[selectedID].classList.add("selected-worker")
+        diagBoxSpace.classList.remove("hidden");
+        footerSpacer.classList.add("hidden");
+    }
 }
-
-
-// UTILIDADES
-function addIdToEachWorker() {
-    availableWorkers.forEach((worker, index) => {
-        worker.setAttribute("id", index);
-    });
-}
-
-// || CALENDARIO
-
-document.addEventListener("click", (e) => {
-  targetEl = e.target;
-  parentEl = targetEl.closest(".worker");
-
-  if (targetEl.classList.contains("calendar-pick-day")){
-    calendarWindow.classList.add("hidden");
-    clientInfoWindow.classList.remove("hidden");
-  }
-})
 
 // || INFORMAÇÕES DO CLIENTE
-
-confirmClientInfoBtn.addEventListener("click", (e) => {
-  clientInfoWindow.classList.add("hidden");
-  scheduleWindow.classList.remove("hidden");
-})
-
-confirmScheduleBtn.addEventListener("click", (e) => {
-  scheduleWindow.classList.add("hidden");
-  serviceWindow.classList.remove("hidden");
-})
-
-confirmServiceBtn.addEventListener("click", (e) => {
-  serviceWindow.classList.add("hidden");
-  overviewWindow.classList.remove("hidden");
-})
-
-function resetWindowPage() {
-  calendarWindow.classList.remove("hidden");
-  clientInfoWindow.classList.add("hidden");
-  scheduleWindow.classList.add("hidden");
-  serviceWindow.classList.add("hidden");
-  overviewWindow.classList.add("hidden");
-}
 
 serviceWindow.addEventListener("click", (e) => {
     targetEl = e.target;
@@ -124,3 +82,37 @@ serviceWindow.addEventListener("click", (e) => {
         }
     }
 })
+
+// || MUDAR DE JANELAS
+
+const windowPages = [ // Agrupar todas as janelas em uma array só
+    calendarWindow,
+    clientInfoWindow,
+    scheduleWindow,
+    serviceWindow,
+    overviewWindow
+]
+
+appointmentWindow.addEventListener("click", (e) => {
+    tarEl = e.target;
+    let buttonID = tarEl.id;
+
+    if (buttonID == "pick-day-btn") { switchBetweenWindows(1); }
+    if (buttonID == "confirm-client-info-btn") { switchBetweenWindows(2); }
+    if (buttonID == "confirm-schedule-btn") { switchBetweenWindows(3); }
+    if (buttonID == "confirm-service-btn") { switchBetweenWindows(4); }
+    if (buttonID == "finish-scheduling") { selectWorker(currentWorkerSel); }
+})
+
+function switchBetweenWindows(index) {
+    windowPages.forEach(page => {
+        page.classList.add("hidden");
+    });
+
+    if (index >= 0) {
+        windowPages[index].classList.remove("hidden"); // exibe a pagina com o numero informado se o index for >= 0
+    }else{
+        windowPages[0].classList.remove("hidden");  // se for -1,mas  reseta para o calendario, e fecha a janela principal
+        appointmentWindow.classList.add("hidden");
+    }
+}
