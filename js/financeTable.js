d1ac75a36ebb84875b8e51f014f73288342fd822
Octavio Lima <octaviolimasan@gmail.com>
2022-11-29 23:20:58 -0300

    tela financeira

--- js/financeTable.js
+++ js/financeTable.js
@@ -7,15 +7,26 @@ const confirmDelEntry = document.querySelector("#confirm-del-button")
 const filterEntryButton = document.querySelector("#filter-entry");
 const financeTable = document.querySelector('table');
 const blockBgElement = document.querySelector(".block-bg-elements");
-const delEntryDiagBox = document.querySelector(".del-entry-diag-box");
+const delEntryDiagBox = document.querySelector("#del-entry-window");
 const cancelDiagBox = document.querySelector("#cancel-button")
 const allDiagWindow = document.querySelectorAll(".dialog-window")
-const addEntryDiagBox = document.querySelector(".add-entry-diag-box")
+const addEntryDiagBox = document.querySelector("#add-entry-diag-box")
 const confirmAddEntryButton = document.querySelector("#confirm-add-entry-button")
 
-const entryNameInputValue = document.querySelector("#entry-name")
+const entryNameInputValue = document.querySelector("#input-entry-name")
 const entryDateInputValue = document.querySelector("#entry-date")
 const entryValueInputValue = document.querySelector("#entry-value")
+const IN_ENTRY_TYPE = document.querySelector("#entry-type")
+
+let accessType = localStorage.getItem("accessType");
+let accessName = localStorage.getItem("accessName");
+
+const date = new Date();
+
+let date_today = date.getDate();
+let date_month = date.getMonth() + 1;
+let date_year = date.getFullYear();
+let currentDate = `${date_today}/${date_month}/${date_year}`;
 
 calculateSumByRow();
 
@@ -43,70 +54,68 @@ document.addEventListener("click", (e) => {
 
 // Calcular Valor Total
 function calculateSumByRow() {
-    // atualizar valores presentes na tabela
-    entryValue = document.querySelectorAll(".value");
-    entrySumValues = document.querySelectorAll(".value-sum");
-    let convertedRowValue = [];
-    let totalSum = [];
+  const EL_TOTAL_PROFIT = document.getElementById("total-profits");
+  let sum = 0;
 
-    // Remover Cifrão e trocar virgulas por pontos
-    entryValue.forEach((rowValue, index) => {
-        let replaceComma = rowValue.textContent.replace(',','.');
-        let removeSign = replaceComma.slice(3);
-        convertedRowValue[index] = parseFloat(removeSign)
-        totalSum[index] = 0; // inicializar array
-    });
+  // atualizar valores presentes na tabela
+  entryValue = document.querySelectorAll(".value");
+  let convertedRowValue = [];
 
-    // Calcular valor total por linha
-    entrySumValues.forEach((rowSumValue, index) => {
-        if (index > 0) {
-            totalSum[index] += (totalSum[index-1] + convertedRowValue[index]);
-        }else{
-            totalSum[index] += (convertedRowValue[index]);
-        }
-        
-        rowSumValue.textContent = ("R$ " + totalSum[index].toFixed(2));
-        rowSumValue.textContent = rowSumValue.textContent.replace('.',',');
-    });
+  // Remover Cifrão e trocar virgulas por pontos
+  entryValue.forEach((rowValue, index) => {
+    let replaceComma = rowValue.textContent.replace(',','.');
+    let removeSign = replaceComma.slice(2);
+    let isDebt = rowValue.closest(".table-row").classList.contains("entry-type-debt");
+    sum += (isDebt ? -parseFloat(removeSign) : parseFloat(removeSign));
+  });
+  //"R$"
+  EL_TOTAL_PROFIT.innerHTML = "Total Mês Atual: " + toMoneyFormat(sum);
 }
 
 // Adicionar novos lançamentos
 function addNewEntry(name, createType, dateCreated, datePayed, createBy, value) {
-    const newEntry = document.createElement("tr");
-    newEntry.classList.add("table-row");
+  const newEntry = document.createElement("tr");
+  newEntry.classList.add("table-row");
 
-    const entryName = document.createElement("td");
-    entryName.innerText = name;
-    newEntry.appendChild(entryName);
+  let isTrue = (createType === 'true');
+  newEntry.classList.add(isTrue ? "entry-type-profit" : "entry-type-debt");
 
-    const entryType = document.createElement("td");
-    entryType.innerText = createType;
-    newEntry.appendChild(entryType);
-    
-    const entryDateCreated = document.createElement("td");
-    entryDateCreated.innerText = dateCreated;
-    newEntry.appendChild(entryDateCreated);
+  const entryName = document.createElement("td");
+  entryName.innerText = name;
+  newEntry.appendChild(entryName);
 
-    const entryDatePayed = document.createElement("td");
-    entryDatePayed.innerText = datePayed;
-    newEntry.appendChild(entryDatePayed);
+  const entryType = document.createElement("td");
+  entryType.innerText = (isTrue ? "Entrada" : "Saída");
+  newEntry.appendChild(entryType);
+  
+  const entryDateCreated = document.createElement("td");
+  entryDateCreated.innerText = dateCreated;
+  newEntry.appendChild(entryDateCreated);
 
-    const entryCreatedBy = document.createElement("td");
-    entryCreatedBy.innerText = createBy;
-    newEntry.appendChild(entryCreatedBy);
+  const entryDatePayed = document.createElement("td");
+  entryDatePayed.innerText = datePayed;
+  newEntry.appendChild(entryDatePayed);
 
-    const entryValue = document.createElement("td");
-    entryValue.innerText = value;
-    entryValue.classList.add("value");
-    newEntry.appendChild(entryValue);
+  const entryCreatedBy = document.createElement("td");
+  entryCreatedBy.innerText = createBy;
+  newEntry.appendChild(entryCreatedBy);
 
-    const entryValueSum = document.createElement("td");
-    entryValueSum.innerText = "R$ 0,00";
-    entryValueSum.classList.add("value-sum");
-    newEntry.appendChild(entryValueSum);
+  const entryValue = document.createElement("td");
+  entryValue.innerText = value;
+  entryValue.classList.add("value");
+  newEntry.appendChild(entryValue);
 
-    financeTable.appendChild(newEntry);
-    calculateSumByRow();
+  const entryValueSum = document.createElement("td");
+  entryValueSum.classList.add("value-sum");
+  const entryShowMoreBtn = document.createElement("button");
+  entryShowMoreBtn.classList.add("fa");
+  entryShowMoreBtn.classList.add("fa-bars");
+  entryShowMoreBtn.classList.add("show-more-btn");
+  entryValueSum.appendChild(entryShowMoreBtn);
+  newEntry.appendChild(entryValueSum);
+
+  financeTable.appendChild(newEntry);
+  calculateSumByRow();
 }
 
 addEntryButton.addEventListener("click", (e) => {
@@ -170,14 +179,7 @@ confirmDelEntry.addEventListener("click", (e) => {
 confirmAddEntryButton.addEventListener("click", (e) => {
     e.preventDefault();
 
-    const date = new Date();
-
-    let day = date.getDate();
-    let month = date.getMonth() + 1;
-    let year = date.getFullYear();
-    let currentDate = `${day}/${month}/${year}`;
-
-    addNewEntry(entryNameInputValue.value,"Manual",currentDate,entryDateInputValue.value, "Admin 8","R$ " + entryValueInputValue.value,"");
+    addNewEntry(entryNameInputValue.value, IN_ENTRY_TYPE.value,currentDate,entryDateInputValue.value, accessName,toMoneyFormat(entryValueInputValue.value),"");
     dialogBoxAdd(false);
 })
 
@@ -202,3 +204,30 @@ cancelDiagBox.addEventListener("click", (e) => {
     e.preventDefault();
 
 })
+
+function toMoneyFormat(value) {
+  let convertToString = "" + value;
+  let number = parseFloat(convertToString, 10); //console.log(number);
+  let addDecimal = number.toFixed(2); //console.log(addDecimal);
+  let replaceDot = addDecimal.replace('.', ','); //console.log(replaceDot);
+
+  let result = convertToString;
+
+  for (let index = addDecimal.length-2; index >= 0; index--) {
+    if (index % 3 === 0) {
+      let splitSta = result.slice(0, index);
+      let splitEnd = result.slice(addDecimal.length-index);
+      result = "" + splitSta + "." + splitEnd;
+      // console.log(index);
+      // console.log("divided by 3");
+      // console.log(splitSta);
+      // console.log(splitEnd);
+      // console.log(addDecimal);
+    }
+  }
+
+  return "R$" + replaceDot;
+}
+
+addNewEntry("primeiro lançamento",'true',"28/11/2020","02/04/2024", accessName,toMoneyFormat(170.30),"");
+addNewEntry("Pagamento Dr. Mario",'false',"01/12/1990","30/12/1994", accessName,toMoneyFormat(900.00),"");
